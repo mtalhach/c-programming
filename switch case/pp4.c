510e91f1dc24a7f02cd7b6fbea15899b08d0586a
HASSAN5077 <talhashafiqch@gmail.com>
2022-12-07 20:01:32 +0500

    c programming

---
+++ switch case/pp4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	char oper;
+	while(1)
+	{
+	printf("\n\nPlz Enter The operator (+,*,-,/): ");
+	fflush(stdin);
+	scanf("%[^\n]",&oper);
+	printf("Plz Enter The numbers:\n");
+	scanf("%d %d",&n1,&n2);
+	
+	switch(oper)
+	{
+		case '+':
+			printf("The Sum of %d and %d is: %d",n1,n2,n1+n2);
+			break;
+		case '-':
+			printf("The Subtraction of %d and %d is: %d",n1,n2,n1-n2);
+			break;
+		case '*':
+			printf("The Multiplication of %d and %d is: %d",n1,n2,n1*n2);
+			break;
+		case '/':
+			printf("The division of %d and %d is: %.2f",n1,n2,n1/(float)n2);
+			break;
+		default:
+			printf("Program terminated");
+	}
+}
+	return 0;
+}
